de0d13b7261ddc149a31bfaab5702b8e2fb16c22
Nicolas De Carlo <kinesiologodecarlo@gmail.com>
2023-06-13 21:20:11 -0500

    Added handlebars and its configuration

--- src/app.js
+++ src/app.js
@@ -1,21 +1,26 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import handlebars from 'express-handlebars';
+import { Server } from "socket.io";
 
-const PORT = 8080
+const PORT = 8080;
 
+app.engine('handlebars', handlebars.engine())
+app.set('views', './src/views')
+app.set('view engine', 'handlebars')
+app.use(express.static('./src/public'))
 
-
-const app = express()
-app.use(express.json())
-
-
-app.use('/api/products', productsRouter)
-app.use('/api/carts', cartsRouter)
-
-
-
+const app = express();
+app.use(express.json());
 
 
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
